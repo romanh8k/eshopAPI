cc92a444b650007f40a3a448a873effa9111cb9e
Roman Horniichuk <romanh8k@gmail.com>
2025-11-27 18:48:49 +0100

    Init

---
+++ src/test/java/main/eshopapi/EshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package main.eshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
