5ade9816d7acdb72c9456ba65ed1dbce263c5d97
Roman Horniichuk <romanh8k@gmail.com>
2025-11-28 23:48:52 +0100

    Add column annotation to product entity class. Add RestController annotation to ProductController

--- src/main/java/main/eshopapi/entities/Product.java
+++ src/main/java/main/eshopapi/entities/Product.java
@@ -12,12 +12,22 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "vendor_id")
     private Vendor vendorId;
+
+    @Column(name = "product_name")
     private String name;
+
+    @Column(name = "product_description")
     private String description;
+
+    @Column(name = "product_price")
     private double price;
+
+    @Column(name = "product_amount")
     private int amount;
 }
